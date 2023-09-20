6f87a877c684376f599cef4a65b85f2e389c8d0b
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2023-09-20 18:14:05 +0200

    clases tailwind css

--- resources/views/prueba/template.blade.php
+++ resources/views/prueba/template.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-  <div class="container mx-auto">
+  <div class="container mx-auto bg-yellow-200 mt-8 p-6 rounded-xl">
     <p>
       Este es el template de prueba para ver!!
     </p>
