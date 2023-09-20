aaff8d68d5f6ea973ff5a73b7306a911d76e122b
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2023-09-20 19:31:38 +0200

    middlewares

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -1,11 +1,15 @@
-<x-guest-layout>
-  <h1>usuarios</h1>
-  @if($users->count() == 0) 
-    <p>No hay usuarios</p>
-  @endif
-  @foreach ($users as $user)
-      <p>
-        {{ $user->id }}.- {{ $user->name }}
-      </p>
-  @endforeach
-</x-guest-layout>
+<x-app-layout>
+  <div class="container mx-auto bg-yellow-200 p-6 rounded-xl mt-8">
+
+    <h1>usuarios</h1>
+    @if($users->count() == 0) 
+      <p>No hay usuarios</p>
+    @endif
+    @foreach ($users as $user)
+        <p>
+          {{ $user->id }}.- {{ $user->name }}
+        </p>
+    @endforeach
+
+  </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/prueba', function () {
+Route::middleware('check.dato')->get('/prueba', function () {
     return view('prueba.template');
 });
 
@@ -27,7 +27,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -35,4 +35,4 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/usuarios', [UserController::class, 'index']);
+Route::get('/usuarios', [UserController::class, 'index'])->middleware(['auth']);
